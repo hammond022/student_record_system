5902e282c4c07144dd64778d86aa8ffeb714c931
hammond022 <75580640+hammond022@users.noreply.github.com>
2024-05-04 16:40:10 +0800

    modified menu_Registration

--- student record.cpp
+++ student record.cpp
@@ -4,16 +4,40 @@
 
 
 struct Student{
-	char id[12];
-	char name[30];
-	char address[50];
-	char birthdate[11];
-	unsigned int age[];
+	struct Personal{
+		char id[12];
+		char first_Name[30];
+		char middle_Name[30];
+		char last_Name[30];
+		
+		char address[50];
+		char birthdate[11];
+		int age;
+		char gender[5]; //m/f
+		
+		char phone_Number[12];
+		char email_Address[30];
+		
+		char mother_First_Name[30];
+		char mother_Middle_Name[30];
+		char mother_Last_Name[30];
+		char mother_Occupation[30];
+	
+		char father_First_Name[30];
+		char father_Middle_Name[30];
+		char father_Last_Name[30];
+		char father_Occupation[30];
+		
+		char guardian_Number[12];
+	}per;
+	
+	
 };
 
 struct Student stu;
 
 void menu_Registration(){
+	char gender;
 	char menu_Choice;
 	
 	system("cls");
@@ -23,12 +47,33 @@ void menu_Registration(){
 	if(fpointer == NULL){
 		printf("Error, cannot write to file.");
 	}else{
-		printf("Student ID:");gets(stu.id);
-		printf("Name:");gets(stu.name);
-		printf("Address:");gets(stu.address);
-		printf("Birthdate MM/DD/YYYY:");gets(stu.birthdate);
-		printf("Age:");scanf("%ld", &stu.age);
+		printf("Student ID: ");gets(stu.per.id);
+		printf("Students First Name: ");gets(stu.per.first_Name);
+		printf("Students Middle Name: ");gets(stu.per.middle_Name);
+		printf("Students Last Name: ");gets(stu.per.last_Name);
+		printf("Address: ");gets(stu.per.address);
+		printf("Birthdate MM/DD/YYYY: ");gets(stu.per.birthdate);
+		
+		printf("Age: ");scanf("%i", &stu.per.age);getchar();
+		
+		printf("Gender: "); gets(stu.per.gender);
+		printf("Phone Number: ");gets(stu.per.phone_Number);
+		printf("Email Address: ");gets(stu.per.email_Address);
+		
+		printf("Mother's First Name: ");gets(stu.per.mother_First_Name);
+		printf("Mother's Middle Name: ");gets(stu.per.mother_Middle_Name);
+		printf("Mother's Last Name: ");gets(stu.per.mother_Last_Name);
+
+		printf("Fathers's First Name: ");gets(stu.per.father_First_Name);
+		printf("Fathers's Middle Name: ");gets(stu.per.father_Middle_Name);
+		printf("Mother's Last Name: ");gets(stu.per.mother_Last_Name);
+		
+		printf("Guardians' Phone Number: ");gets(stu.per.guardian_Number);
+		
+		
+		
 		fwrite(&stu, sizeof(stu), 1, fpointer); // no error checking
+		
 		printf("\nDo you want to save this student to the database? [y/n]");menu_Choice = getche();
 		if(menu_Choice != 'n'){
 			printf("\nStudent successfuly added to database.");
@@ -38,17 +83,11 @@ void menu_Registration(){
 			printf("\nPress any key to go back to main menu.");getch();return;
 		}
 	}
-	
-	
-
 }
 
 
 int main(){
-	
-	
 	char choice;
-	
 	do{
 		system("cls");
 		printf("\nA.Registration");
@@ -68,9 +107,7 @@ int main(){
 		default: 
 			printf("Invalid option");
 			break;
-		
 		}
-		
 	}while(choice != 'e' );
 	return 0;
 }
