bf369f654083209cd3771b562bc83ccf2191946f
hammond022 <75580640+hammond022@users.noreply.github.com>
2024-05-07 03:21:17 +0800

    Update student record.cpp
    Registration and Enrollment has been polished, Payment still needs some work,  Reports still not implemented
    
    structure should resemble its final form.

--- student record.cpp
+++ student record.cpp
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <string.h>
+#include <math.h>
 
+#define BLANK " "
+char blank[] = BLANK;
 
 struct Student{
 	struct Personal{
@@ -38,86 +41,192 @@ struct Student{
 		char has_Diploma;
 		char has_Form137;
 		char has_ReportCard;
-	};
+	}doc;
 	
 	struct Enrollment{
-		char strand;  // 1.STEM 2.HUMSS 3.ABM 4.GAS
-		char yearlvl; // 1 = 11, 2=12
-		char semester; // A = first sem, B = second sem.
-		char section; // 1 , 2
+		int strand;  // 0.STEM 1.HUMSS 2.ABM 3.GAS
+		int yearlvl; // 11 = 11, 12=12
+		int semester; // 1 = first sem, 2 = second sem.
+		int section; // 1 , 2
 	}enr;
 	
 	struct Payment{
-		int 
-	};
+		float outstandingbal;
+	}pay;
+	
+	struct flags{
+		int isEnrolled;
+	}flg;
 };
-
 struct Student stu;
+char choice;
 
+void pause(){
+	printf("\n");
+	system("pause");
+	
+}
+void continue_Choice(char cntChoice){
+	choice = cntChoice;
+	return;
+}
 void clear_Screen(){
 	system("cls");
 	return;
 }
+void call_Error(int mode){
+	switch (mode){
+		case 1:
+			printf("\nError Cannot write to file.");
+			break;
+		case 6:
+			printf("This student is already enrolled");
+			break;
+		default:
+			break;
+	}
+}
+char print_Modal(char header[], int type){
+	int c, i;
+	switch(type) {
+		case 1:
+			for(i = 0; i <95 ; i++ )printf("=");printf("\n");
+			for(i = 0; i <95 ; i++ ){if(i < 2)printf("|");else if( i > 92)printf("|");else printf("%s", blank);} printf("\n");
+			c = ceil(45  - (strlen(header) / 2)); printf("||");for(i = 0; i < c ; i++ )printf(" "); printf(header);  for(i = 0; i < c ; i++ )printf("%s", blank);printf("||\n");
+			for(i = 0; i <95 ; i++ ){if(i < 2)printf("|");else if( i > 92)printf("|");else printf("%s", blank);} printf("\n");
+			for(i = 0; i <95 ; i++ )printf("=");printf("\n");
+		break;
+		
+		case 2: 
+			c = ceil(45  - (strlen(header) / 2)); printf("==");for(i = 0; i < c ; i++ )printf(" "); printf(header);  for(i = 0; i < c ; i++ )printf("%s", blank);printf("==\n\t\t\t\t\t>");
+		break;
+	}
+}
 
-
-// no error checking come back to this
+// no more issues 
 void menu_Registration(){
+	system("cls");
+	print_Modal("Registration ", 1);
+	
 	char gender;
 	char menu_Choice;
 	
-	system("cls");
 	FILE *fpointer;
 	fpointer = fopen("record.txt", "ab+");
 	
 	if(fpointer == NULL){
-		printf("Error, cannot write to file.");
+		call_Error(1);
 	}else{
-		printf("Student ID: ");gets(stu.per.id);
-		printf("Students First Name: ");gets(stu.per.first_Name);
-		printf("Students Middle Name: ");gets(stu.per.middle_Name);
-		printf("Students Last Name: ");gets(stu.per.last_Name);
-		printf("Address: ");gets(stu.per.address);
-		printf("Birthdate MM/DD/YYYY: ");gets(stu.per.birthdate);
-		
-		printf("Age: ");scanf("%i", &stu.per.age);getchar();
-		
-		printf("Gender: "); gets(stu.per.gender);
-		printf("Phone Number: ");gets(stu.per.phone_Number);
-		printf("Email Address: ");gets(stu.per.email_Address);
-		
-		printf("Mother's First Name: ");gets(stu.per.mother_First_Name);
-		printf("Mother's Middle Name: ");gets(stu.per.mother_Middle_Name);
-		printf("Mother's Last Name: ");gets(stu.per.mother_Last_Name);
+		print_Modal("Student ID ex.[123456789000]:", 2);gets(stu.per.id);
+		print_Modal("Students First Name:", 2);gets(stu.per.first_Name);
+		print_Modal("Students Middle Name:", 2);gets(stu.per.middle_Name);
+		print_Modal("Students Last Name:", 2);gets(stu.per.last_Name);
+		print_Modal("Address:", 2);gets(stu.per.address);
+		print_Modal("Birthdate ex.[MM/DD/YYYY]:", 2);gets(stu.per.birthdate);
+		print_Modal("Age:", 2);scanf("%i", &stu.per.age);getchar(); // ??? gumagana to idk why
+		print_Modal("Gender [male/female]:", 2);gets(stu.per.gender);
+		print_Modal("Phone Number ex.[09123456789]:", 2);gets(stu.per.phone_Number);
+		print_Modal("Email Address:", 2);gets(stu.per.email_Address);
+		print_Modal("Mother's First Name:", 2);gets(stu.per.mother_First_Name);
+		print_Modal("Mother's Middle Name:", 2);gets(stu.per.mother_Middle_Name);
+		print_Modal("Mother's Last Name:", 2);gets(stu.per.mother_Last_Name);
+		print_Modal("Fathers's First Name:", 2);gets(stu.per.father_First_Name);
+		print_Modal("Fathers's Middle Name:", 2);gets(stu.per.father_Middle_Name);
+		print_Modal("Mother's Last Name:", 2);gets(stu.per.mother_Last_Name);
+		print_Modal("Guardians' Phone Number ex.[09123456789]:", 2);gets(stu.per.guardian_Number);
+		print_Modal("Do you want to save this student to the database? [y/n]", 2);menu_Choice = getche();
 
-		printf("Fathers's First Name: ");gets(stu.per.father_First_Name);
-		printf("Fathers's Middle Name: ");gets(stu.per.father_Middle_Name);
-		printf("Mother's Last Name: ");gets(stu.per.mother_Last_Name);
-		
-		printf("Guardians' Phone Number: ");gets(stu.per.guardian_Number);
-		
-		
-		
-		fwrite(&stu, sizeof(stu), 1, fpointer); // no error checking
-		
-		printf("\nDo you want to save this student to the database? [y/n]");menu_Choice = getche();
 		if(menu_Choice != 'n'){
-			printf("\nStudent successfuly added to database.");
-			printf("\nPress any key to go back to main menu.");getch();return;
+				if(fwrite(&stu, sizeof(stu), 1, fpointer) == 1){printf("\nStudent successfuly added to database.\n");
+				print_Modal("Do you want to continue to Enrollment? [y/n]", 2);menu_Choice = getch();
+					if(menu_Choice == 'y'){continue_Choice('2');}
+				}else{call_Error(1);}
 		}else{
-			printf("\nStudent not added to database.");
-			printf("\nPress any key to go back to main menu.");getch();return;
+		printf("\nStudent not added to database.");
+		continue_Choice('0');;
 		}
 	}
+	
 	fclose(fpointer);
 	fflush(fpointer);
+	system("pause");
+	return;
 }
 
 // no error checking come back to this
 void menu_Enrollment(){
-	clear_Screen();
+	system("cls");
+	print_Modal("Enrollment ", 1);
+	
 	char search_Student_Id[12];
 	int bolis_Found = 0;
-	char choice = 0;
+	char menu_Choice;
+	char strands[][10] = {"STEM", "HUMSS", "ABM", "GAS"};
+	char semester[3] = {'X', 'A', 'B'};
+	
+	if(choice != '2' && choice != '2'){
+		print_Modal("Enter the ID of Student: ", 2);gets(search_Student_Id);
+	}else{strcpy(search_Student_Id, stu.per.id);};
+
+	FILE *fpointer;
+	fpointer = fopen("record.txt", "rb+");
+	while(fread(&stu, sizeof(stu), 1, fpointer) == 1){
+		if(strcmp(search_Student_Id, stu.per.id) == 0){
+			bolis_Found = 1;break;}}
+	
+	if(bolis_Found == 1){
+		print_Modal("Student Name:", 2);printf("%s, %s %s\n", stu.per.last_Name, stu.per.first_Name, stu.per.middle_Name);
+		print_Modal("Address: ", 2);printf("%s\n", stu.per.address);
+		print_Modal("Birthdate: ", 2);printf("%s\n", stu.per.birthdate);
+		print_Modal("Age: ", 2);printf("%i\n", stu.per.age);
+		
+		if(stu.flg.isEnrolled == 1){
+		print_Modal("STRAND:", 2);printf("%s\n", strands[stu.enr.strand]);
+		print_Modal("SEMESTER:", 2);printf("%c\n", semester[stu.enr.semester]);
+		print_Modal("YEAR:" , 2);printf("%i\n", stu.enr.yearlvl);
+		print_Modal("SECTION: ", 2);printf("%i\n", stu.enr.section);
+		call_Error(6);
+		continue_Choice('0');
+		
+		}else{
+			
+			print_Modal("Enter strand: [0 = STEM / 1 = HUMSS / 2 = ABM / 3 = GAS]:", 2);scanf("%i", &stu.enr.strand);
+			print_Modal("Enter year level: [11 / 12]",2);scanf("%i", &stu.enr.yearlvl);
+			print_Modal("Enter semester: [1 / 2]",2);scanf("%i", &stu.enr.semester);
+			print_Modal("Enter Section: [1 / 2]",2);scanf("%i", &stu.enr.section);
+			print_Modal("Do you want to enroll now? [y/n]",2);menu_Choice = getche();
+			if(menu_Choice == 'y'){
+				continue_Choice('3');
+				stu.pay.outstandingbal = 26731;
+				stu.flg.isEnrolled = 1;
+				fseek(fpointer, -sizeof(stu), SEEK_CUR);
+				fwrite(&stu, sizeof(stu), 1, fpointer);
+				printf("\nStudent enrolled successfully");
+			}else{printf("\nStudent not enrolled.");continue_Choice('0');}
+		}
+	}else{printf("No records found!");continue_Choice('0');}
+	
+	
+	fclose(fpointer);
+	fflush(fpointer);
+	pause();
+	return;
+}
+
+
+
+
+
+void menu_Payment(){
+	clear_Screen();
+	
+	char search_Student_Id[12];
+	int bolis_Found = 0;
+	char menu_Choice = 0;
+	float receiveAmnt;
+	
+	char strands[][10] = {"STEM", "HUMSS", "ABM", "GAS"};
+	char semester[3] = {'X', 'A', 'B'};
 	
 	printf("Enter the ID of Student: ");gets(search_Student_Id);
 	
@@ -131,36 +240,43 @@ void menu_Enrollment(){
 	}
 	
 	if(bolis_Found == 1){
-		printf("\nStudent is found!");
+		printf("Student is found");
 		printf("\nStudent Name: %s, %s %s", stu.per.last_Name, stu.per.first_Name, stu.per.middle_Name);
 		printf("\nAddress: %s", stu.per.address);
 		printf("\nBirthdate: %s", stu.per.birthdate);
 		printf("\nAge: %i", stu.per.age);
-		getch();
 		
-		printf("\nEnter strand: [1 = STEM / 2 = HUMSS / 3 = ABM / 4 = GAS]: ");stu.enr.strand= getche();
-		printf("\nEnter year level: ");stu.enr.yearlvl = getche();
-		printf("\nEnter semester: ");stu.enr.semester = getche();
-		printf("\nEnter Section: ");stu.enr.section = getche();
-		printf("\nDo you want to enroll now? [y/n]");choice = getch();
-		if(choice == 'y'){
-			fseek(fpointer, -sizeof(stu), SEEK_CUR);
-			fwrite(&stu, sizeof(stu), 1, fpointer);
+		printf("\nSTRAND: %s", strands[stu.enr.strand] );
+		printf("\nSEMESTER: %c", semester[stu.enr.semester]);
+		printf("\nYEAR: %i", stu.enr.yearlvl);
+		printf("\nSECTION: %i", stu.enr.section);
 		
-		}
-	}else{
-		printf("No records found!");
-		getch();
+		
 	}
+	
+	
+	printf("\n*Miscellaneous Fees*\nLearning Management Fee             = PHP 850\nMedical Services Fee                = PHP 557\nRegistration Fee                    = PHP 749\nGuidance and Counseling Fee         = PHP 642\nManangement Information System Fee  = PHP 1,079\nLibrary Fee                         = PHP 350\n*Tuition Fee*\nLaboratory Fee                      = PHP 3,342\nSemester Fee (Gened)                = PHP 11,323\nSemester Fee (Specialized)          = PHP 7,839");
+	printf("\n\nGRAND TOTAL = PHP 26,731\n");
+	print_Modal("Outstanding Balance: ", 2);
+	printf("%.2f\n", stu.pay.outstandingbal);
+	print_Modal("Receive Amount:  ", 2);scanf("%f", &receiveAmnt);
+	print_Modal("Change: ", 2);
+	if(receiveAmnt > stu.pay.outstandingbal){
+		printf("%.2f\n", receiveAmnt-stu.pay.outstandingbal);
+		stu.pay.outstandingbal = 0;
+	}else{
+		printf("No change [%.2f Credited to account]\n", receiveAmnt);
+		stu.pay.outstandingbal -= receiveAmnt;
+		print_Modal("Outstanding Balance: ", 2);
+		printf("%.2f", stu.pay.outstandingbal);}
+	
+	fseek(fpointer, -sizeof(stu), SEEK_CUR);
+	fwrite(&stu, sizeof(stu), 1, fpointer);
 	
 	fclose(fpointer);
 	fflush(fpointer);
+	pause();
 	return;
-}
-
-void menu_Payment(){
-	clear_Screen();
-	
 }
 
 void debug_menu_Search(){
@@ -200,19 +316,28 @@ void debug_menu_Search(){
 	return;
 }
 
+void debug_Flex(){
+	clear_Screen();
+			printf("\n*Miscellaneous Fees*\nLearning Management Fee             = PHP 850\nMedical Services Fee                = PHP 557\nRegistration Fee                    = PHP 749\nGuidance and Counseling Fee         = PHP 642\nManangement Information System Fee  = PHP 1,079\nLibrary Fee                         = PHP 350\n*Tuition Fee*\nLaboratory Fee                      = PHP 3,342\nSemester Fee (Gened)                = PHP 11,323\nSemester Fee (Specialized)          = PHP 7,839");
+	printf("\n\nGRAND TOTAL = PHP 26,731");
+	
+		system("pause");
+		return;
+}
 
 int main(){
-	char choice;
 	do{
+		if(choice != '2' && choice != '3'){
 		system("cls");
+		printf("test%c", choice);
 		printf("\nA.Registration");
 		printf("\nB.Enrollment");
 		printf("\nC.Payment");
 		printf("\nD.Reports");
 		printf("\nE.Exit");
-		
 		printf("\nEnter your choice: ");choice = getche();
-		
+		}
+
 		switch (choice){
 		case 'a':
 		case 'A':
@@ -220,11 +345,21 @@ int main(){
 			break;
 		case 'b':
 		case 'B':
+		case '2':
 			menu_Enrollment();
+			break;
+		case 'c':
+		case 'C':
+		case '3':
+			menu_Payment();
 			break;
 		case 'x': //debug
 		case 'X':
 			debug_menu_Search();
+			break;
+		case 'y': //debug
+		case 'Y':
+			debug_Flex();
 			break;
 		default: 
 			printf("Invalid option");
